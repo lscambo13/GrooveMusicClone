42b3c6d4b88c77636319de99570274c4a571e2ca
msc24x <msc24x@gmail.com>
2021-02-15 20:40:26 +0530

    Introduce SharedPreferences
    Signed-off-by: lscambo13 <ls.cambo13@gmail.com>

---
+++ app/src/main/java/com/msc24x/player/Preferences.kt
@@ -0,0 +1,39 @@
+package com.msc24x.player
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class Preferences(context: Context) {
+
+//    Theme settings begin. --------------------------------
+
+    // This is the save file for theme settings.
+    val preferenceProgressFile = "ConfigProgress"
+
+    // This is the integer entry for theme style.
+    val preferenceIDProgress = "CurrentProgress"
+
+    // This reads the theme file, ConfigTheme,
+    // and reads the presently set theme style from CurrentTheme. (default = set by system)
+    //
+    // Integer values are as follows
+    // MODE_NIGHT_FOLLOW_SYSTEM = -1
+    // MODE_NIGHT_NO = 1
+    // MODE_NIGHT_YES = 2
+    val preferenceProgress: SharedPreferences =
+        context.getSharedPreferences(preferenceProgressFile, Context.MODE_PRIVATE)
+
+    fun getCurrentProgress(): Int {
+        return preferenceProgress.getInt(preferenceIDProgress, 0)
+    }
+
+    // This saves the set theme as -1, 1 or 2.
+    fun setCurrentProgress(progress: Int) {
+        val editor = preferenceProgress.edit()
+        editor.putInt(preferenceIDProgress, progress)
+        editor.apply()
+    }
+
+//  Theme settings end. --------------------------------
+
+}
