ba362be260d9927d1337e80d6072206b6119888f
msc24x <msc24x@gmail.com>
2021-02-15 20:38:57 +0530

    Workaround for music issues when layout updates
    Signed-off-by: lscambo13 <ls.cambo13@gmail.com>

--- app/src/main/java/com/msc24x/player/MainActivity.kt
+++ app/src/main/java/com/msc24x/player/MainActivity.kt
@@ -2,24 +2,47 @@ package com.msc24x.player
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.res.Configuration
+import android.graphics.Color
 import android.media.MediaPlayer
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
+import android.view.*
 import android.widget.SeekBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var player: MediaPlayer
+    var safeThread = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // This makes the actionbar flat
+        // supportActionBar?.elevation = 0F
+
+        //makeStatusBarTransparent()
+        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragmentContainerMain)) { _, insets ->
+        //    findViewById<TextView>(R.id.materialToolbar).setMarginTop(insets.systemWindowInsetTop)
+        //    insets.consumeSystemWindowInsets()
+        //}
+
+
         val songsFragment = SongsFragment()
         val artistsFragment = ArtistsFragment()
         val albumsFragment = AlbumsFragment()
+        //switchTab(songsFragment)
+
+        viewPager.adapter = ViewPagerAdapter(songsFragment)
 
         ActivityCompat.requestPermissions(
             this,
@@ -27,24 +50,36 @@ class MainActivity : AppCompatActivity() {
             111
         )
 
-        switchTab(songsFragment)
+        var songUri = R.raw.song
 
-        btnPlay.setBackgroundResource(R.drawable.pausebtn)
-        val player: MediaPlayer = MediaPlayer.create(this, R.raw.song)
+        player = MediaPlayer.create(this, songUri)
+        player.isLooping = true
 
-        player.setVolume(0.5f, 0.5f)
-        btnPlay.setBackgroundResource(R.drawable.playbtn)
         tvSongName.text = "Gotta Be A Reason"
         tvArtistName.text = "Alec Benjamin"
-        tvTrackLength.text = progresstoString(player.duration)
+        tvTrackLength.text = progressToString(player.duration)
+        player.setVolume(0.5f, 0.5f)
+        btnPlay.setBackgroundResource(R.drawable.playbtn)
+
+        var isPlaying = true
+        var progressPos = Preferences(this).getCurrentProgress()
+
+        player.seekTo(progressPos)
+        seekbar.progress = progressPos
+        if (isPlaying) {
+            //player.start()
+            btnPlay.setBackgroundResource(R.drawable.pausebtn)
+        }
 
         btnPlay.setOnClickListener {
             if (player.isPlaying) {
                 player.pause()
                 btnPlay.setBackgroundResource(R.drawable.playbtn)
+                savedInstanceState?.putBoolean("isPlaying", false)
             } else {
                 btnPlay.setBackgroundResource(R.drawable.pausebtn)
                 player.start()
+                savedInstanceState?.putBoolean("isPlaying", true)
             }
         }
         seekbar.max = player.duration
@@ -57,7 +92,7 @@ class MainActivity : AppCompatActivity() {
                 ) {
                     if (fromUser) {
                         player.seekTo(progress)
-                        tvTimeCode.text = progresstoString(progress)
+                        tvTimeCode.text = progressToString(progress)
                     }
                 }
 
@@ -70,37 +105,87 @@ class MainActivity : AppCompatActivity() {
         var handler = object : Handler() {
             override fun handleMessage(msg: Message) {
                 seekbar.progress = msg.what
-                tvTimeCode.text = progresstoString(msg.what)
+                tvTimeCode.text = progressToString(msg.what)
+                savedInstanceState?.putInt("progressPos", msg.what)
             }
         }
 
-
-        Thread(
-            Runnable {
-                while (player != null) {
-                    try {
-                        var msg = Message()
-                        msg.what = player.currentPosition
-                        handler.sendMessage(msg)
-                        Thread.sleep(100)
-                    } catch (e: InterruptedException) {
-                    }
-                }
-            }).start()
-
+        Thread {
+            while (safeThread) {
+                var msg = Message()
+                msg.what = player.currentPosition
+                Preferences(this).setCurrentProgress(msg.what)
+                handler.sendMessage(msg)
+                Thread.sleep(100)
+            }
+        }.start()
     }
 
-    private fun switchTab(fragment: Fragment) = supportFragmentManager.beginTransaction().apply {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menuSearch -> {
+                println("Searched.")
+                Toast.makeText(applicationContext, "Searched.", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return true
+    }
+
+
+    fun switchTab(fragment: Fragment) = supportFragmentManager.beginTransaction().apply {
         replace(R.id.fragmentContainerMain, fragment)
         commit()
     }
 
-    fun progresstoString(p: Int): String {
+    // Experiment for transparent status bar
+    private fun Activity.makeStatusBarTransparent() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.apply {
+                clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+                addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    val mode =
+                        context?.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)
+                    when (mode) {
+                        Configuration.UI_MODE_NIGHT_YES -> {
+                            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        }
+                        Configuration.UI_MODE_NIGHT_NO -> {
+                            decorView.systemUiVisibility =
+                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                        }
+                        Configuration.UI_MODE_NIGHT_UNDEFINED -> {
+                            decorView.systemUiVisibility =
+                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                        }
+                    }
+                } else {
+                    decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                }
+                statusBarColor = Color.TRANSPARENT
+            }
+        }
+    }
+
+    private fun View.setMarginTop(marginTop: Int) {
+        val menuLayoutParams = this.layoutParams as ViewGroup.MarginLayoutParams
+        menuLayoutParams.setMargins(0, marginTop, 0, 0)
+        this.layoutParams = menuLayoutParams
+    }
+
+    fun progressToString(p: Int): String {
         var str = ""
         val min = p / 1000 / 60
         val sec = p / 1000 % 60
-        if (min < 10) {
-            str = "0" + min.toString() + ":"
+        if (min == 0) {
+            str += "0:"
+        } else if (min < 10) {
+            str = min.toString() + ":"
         } else {
             str = min.toString() + ":"
         }
@@ -112,6 +197,15 @@ class MainActivity : AppCompatActivity() {
         return str
     }
 
+    fun setTvSongName(song: String) {
+        this.tvSongName.text = song
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        safeThread = false
+        player.release()
+    }
 }
 
 
