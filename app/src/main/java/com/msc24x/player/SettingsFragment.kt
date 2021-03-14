e27090d4da655736e61a0ee794a785bb61d81563
lscambo13 <ls.cambo13@gmail.com>
2021-03-14 16:11:58 +0530

    Add preferences fragment (WIP)

---
+++ app/src/main/java/com/msc24x/player/SettingsFragment.kt
@@ -0,0 +1,11 @@
+package com.msc24x.player
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+}
