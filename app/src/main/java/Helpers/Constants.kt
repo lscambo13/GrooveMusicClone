aae2531168493509733cba21bad0a50896e89574
msc24x <msc24x@gmail.com>
2021-05-19 19:46:43 +0530

    Rewrite PlayerService.kt

---
+++ app/src/main/java/Helpers/Constants.kt
@@ -0,0 +1,8 @@
+package Helpers
+
+const val PLAY = "play"
+const val PLAY_SONG = "play_song"
+const val PAUSE = "pause"
+const val INIT = "init"
+const val SEEK_TO = "seek_to"
+const val TRACK_URI = "track_uri"
--- app/src/main/java/com/msc24x/player/mediaplayer/PlayerService.kt
+++ app/src/main/java/com/msc24x/player/mediaplayer/PlayerService.kt
@@ -1,32 +1,84 @@
 package com.msc24x.player.mediaplayer
 
+import Helpers.*
 import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.IBinder
+import android.util.Log
 
 
 class PlayerService : Service() {
 
-    val CHANNEL_ID = "NowPlayingChannel"
     lateinit var player: MediaPlayer
-    var safeThread = true
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        //Toast.makeText(applicationContext, "service started", Toast.LENGTH_SHORT).show()
+        safeInit()
+        when (intent!!.action) {
+            PLAY_SONG -> playSong(intent)
+            PLAY -> play()
+            PAUSE -> pause()
+            SEEK_TO -> seekTo(intent)
+            INIT -> safeInit()
+        }
+
         return START_STICKY
     }
+
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
 
-    fun play(uri: Uri) {
-        player.pause()
+    private fun playSong(intent: Intent) {
+        setUri(intent)
+        play()
+    }
+
+    private fun play() {
+        Log.v("play", player.isPlaying.toString())
+        if (!player.isPlaying) {
+            player.start()
+        }
+        broadcastBusy(true)
+    }
+
+    private fun pause() {
+        if (player.isPlaying)
+            player.pause()
+        broadcastBusy(false)
+    }
+
+    private fun seekTo(intent: Intent) {
+        val pos = intent.getIntExtra(SEEK_TO, -1)
+        if (pos != -1)
+            player.seekTo(pos)
+    }
+
+    private fun safeInit() {
+        if (this::player.isInitialized)
+            return
+        player = MediaPlayer()
+        player.isLooping = true
+        broadcastBusy(false)
+    }
+
+    private fun setUri(intent: Intent) {
         player.reset()
-        player = MediaPlayer.create(applicationContext, uri)
-        player.start()
+        val uri = Uri.parse(intent.getStringExtra(TRACK_URI))
+        player = MediaPlayer.create(applicationContext, uri!!)
+        player.isLooping = true
+        //player.prepare()
+    }
+
+
+    private fun broadcastBusy(busy: Boolean) {
+        //CommonViewModel().busy.value = busy
+    }
+
+    private fun broadcastPos() {
+
     }
 
 }
