95a2ebcff1f0edf7c753b1df47b65a883cbd1cae
lscambo13 <ls.cambo13@gmail.com>
2021-05-27 14:23:31 +0530

    Remove blur related functions Not interested anymore

--- app/src/main/java/com/msc24x/player/MainActivity.kt
+++ app/src/main/java/com/msc24x/player/MainActivity.kt
@@ -87,42 +87,6 @@ class MainActivity : AppCompatActivity() {
         }
         return true
     }
-
-
-    fun switchTab(fragment: Fragment) = supportFragmentManager.beginTransaction().apply {
-        //replace(R.id.fragmentContainerMain, fragment)
-        //commit()
-    }
-
-
-    private fun enableBlur() {
-        val radius = 15f
-        val decorView = window.decorView
-        //ViewGroup you want to start blur from. Choose root as close to BlurView in hierarchy as possible.
-        val rootView = decorView.findViewById(android.R.id.content) as ViewGroup
-        //Set drawable to draw in the beginning of each blurred frame (Optional).
-        //Can be used in case your layout has a lot of transparent space and your content
-        //gets kinda lost after after blur is applied.
-        //Set drawable to draw in the beginning of each blurred frame (Optional).
-        //Can be used in case your layout has a lot of transparent space and your content
-        //gets kinda lost after after blur is applied.
-        val windowBackground = decorView.background
-        blurMiniPlayer.setupWith(rootView)
-            .setFrameClearDrawable(windowBackground)
-            .setBlurAlgorithm(RenderScriptBlur(this))
-            .setBlurRadius(radius)
-            .setBlurAutoUpdate(true)
-            .setHasFixedTransformationMatrix(true)
-        blurAppBar.setupWith(rootView)
-            .setFrameClearDrawable(windowBackground)
-            .setBlurAlgorithm(RenderScriptBlur(this))
-            .setBlurRadius(radius)
-            .setBlurAutoUpdate(true)
-            .setHasFixedTransformationMatrix(true)
-
-    }
-
-
 }
 
 
