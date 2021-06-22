47504dd03b11dc88ddba7019a98e37d8959b9a5d
msc24x <msc24x@gmail.com>
2021-06-22 17:09:02 +0530

    Update app actions' strings

--- app/src/main/java/Helpers/Constants.kt
+++ app/src/main/java/Helpers/Constants.kt
@@ -1,9 +1,9 @@
 package Helpers
 
-const val PLAY = "play"
-const val PLAY_SONG = "play_song"
-const val PAUSE = "pause"
-const val INIT = "init"
-const val SEEK_TO = "seek_to"
-const val TRACK_URI = "track_uri"
-const val END_SESSION = "end_session"
+const val PLAY = "com.msc24x.player.action.PLAY"
+const val PLAY_SONG = "com.msc24x.player.action.PLAY_SONG"
+const val PAUSE = "com.msc24x.player.action.PAUSE"
+const val INIT = "com.msc24x.player.action.INIT"
+const val SEEK_TO = "com.msc24x.player.action.SEEK_TO"
+const val TRACK_URI = "com.msc24x.player.action.TRACK_URI"
+const val END_SESSION = "com.msc24x.player.action.END_SESSION"
