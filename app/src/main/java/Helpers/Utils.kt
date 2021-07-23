abeaaf9fc7ffa75e17f91891202cb60b2bc95cc3
msc24x <msc24x@gmail.com>
2021-07-23 20:24:41 +0530

    Add extractMutedColor()

--- app/src/main/java/Helpers/Utils.kt
+++ app/src/main/java/Helpers/Utils.kt
@@ -1,24 +1,32 @@
 package Helpers
 
+import android.graphics.Bitmap
+import android.graphics.Color
+import androidx.palette.graphics.Palette
+
 class Utils {
     companion object {
         fun progressToString(p: Int): String {
             var str = ""
             val min = p / 1000 / 60
             val sec = p / 1000 % 60
-            if (min == 0) {
+            if (min == 0)
                 str += "0:"
-            } else if (min < 10) {
+            else if (min < 10)
                 str = "$min:"
-            } else {
+            else
                 str = "$min:"
-            }
-            if (sec < 10) {
+            if (sec < 10)
                 str = str + "0" + sec.toString()
-            } else {
+            else
                 str += sec.toString()
-            }
             return str
+        }
+
+        fun extractMutedColor(art: Bitmap): Int {
+            val myPalette = Palette.from(art).generate()
+            val muted = myPalette.mutedSwatch ?: myPalette.darkVibrantSwatch
+            return muted?.rgb ?: Color.DKGRAY
         }
     }
 }
