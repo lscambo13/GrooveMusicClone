a06f84bd9b253e86c6c7d077b0aea3ab664e93dc
msc24x <msc24x@gmail.com>
2021-07-23 20:28:50 +0530

    Add new constants

--- app/src/main/java/Helpers/Constants.kt
+++ app/src/main/java/Helpers/Constants.kt
@@ -1,9 +1,14 @@
 package Helpers
 
-const val PLAY = "com.msc24x.player.action.PLAY"
-const val PLAY_SONG = "com.msc24x.player.action.PLAY_SONG"
-const val PAUSE = "com.msc24x.player.action.PAUSE"
-const val INIT = "com.msc24x.player.action.INIT"
-const val SEEK_TO = "com.msc24x.player.action.SEEK_TO"
-const val TRACK_URI = "com.msc24x.player.action.TRACK_URI"
-const val END_SESSION = "com.msc24x.player.action.END_SESSION"
+object Constants {
+    const val PLAY = "com.msc24x.player.action.PLAY"
+    const val PREV = "com.msc24x.player.action.PREV"
+    const val NEXT = "com.msc24x.player.action.NEXT"
+    const val PLAY_SONG = "com.msc24x.player.action.PLAY_SONG"
+    const val PAUSE = "com.msc24x.player.action.PAUSE"
+    const val INIT = "com.msc24x.player.action.INIT"
+    const val SEEK_TO = "com.msc24x.player.action.SEEK_TO"
+    const val TRACK_URI = "com.msc24x.player.action.TRACK_URI"
+    const val END_SESSION = "com.msc24x.player.action.END_SESSION"
+    const val SET_PLAYLIST = "com.msc24x.player.action.SET_PLAYLIST"
+}
